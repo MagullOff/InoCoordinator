25339941cb1b7f89697baccfaae27aa804dafc74
magulloff <przemczar2001@gmail.com>
2023-01-10 23:58:58 +0100

    model SubmissionForm

---
+++ lib/shared/submission_form.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../themes.dart';
+
+class SubmissionForm extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  final String title;
+  final Widget input;
+  const SubmissionForm(
+      {super.key,
+      required this.formKey,
+      required this.title,
+      required this.input});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: Themes.textTheme().headline2,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                input
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
