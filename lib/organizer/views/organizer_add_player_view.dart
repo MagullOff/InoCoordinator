b478b1cbf6377a77b7e9de7df3d3de136fad7ebd
magulloff <przemczar2001@gmail.com>
2023-01-11 00:03:32 +0100

    add add player view

--- lib/organizer/views/organizer_add_player_view.dart
+++ lib/organizer/views/organizer_add_player_view.dart
@@ -6,6 +6,8 @@ import 'package:ino_coordinator/organizer/bloc/organizer_bloc.dart';
 import 'package:ino_coordinator/shared/page_with_watermark.dart';
 import 'package:ino_coordinator/themes.dart';
 
+import '../../shared/submission_form.dart';
+import '../../shared/text_input_field.dart';
 import '../../shared/wide_button.dart';
 
 class OrganizerAddPlayerView extends StatelessWidget {
@@ -18,8 +20,34 @@ class OrganizerAddPlayerView extends StatelessWidget {
         appBar: Themes.defaultAppBar(),
         child: Stack(alignment: Alignment.bottomCenter, children: [
           _submitButton(),
-          //_addPlayerForm(),
+          _playerForm(),
         ]));
+  }
+
+  Widget _playerForm() {
+    return SubmissionForm(
+      formKey: _formKey,
+      title: 'Enter username of the new player',
+      input: _usernameField(),
+    );
+  }
+
+  Widget _usernameField() {
+    return BlocBuilder<OrganizerBloc, OrganizerState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(0, 10, 0, 5),
+          child: TextInputField(
+            inputType: InputType.numeric,
+            validator: (value) => null,
+            onChanged: (value) => {
+              //TODO: logic
+            },
+            decorationText: "Username",
+          ),
+        );
+      },
+    );
   }
 
   Widget _submitButton() {
