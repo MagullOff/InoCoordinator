371c3e4cfae8bf15791228c469b56375a243567a
magulloff <przemczar2001@gmail.com>
2023-01-16 22:56:42 +0100

    formatting fix

--- lib/organizer/views/organizer_error_view.dart
+++ lib/organizer/views/organizer_error_view.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:ino_coordinator/shared/components/page_with_watermark.dart';
 import 'package:ino_coordinator/themes.dart';
 
